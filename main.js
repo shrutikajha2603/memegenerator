2cd8cb8758fd3b4fdea4d54f99b745493af04338
Shrutika Jha <shrutikajha2603@gmail.com>
2023-11-10 21:08:24 +0000

    final

--- main.js
+++ main.js
@@ -4,14 +4,30 @@ const generateMemeBtn = document.querySelector(
   const memeImage = document.querySelector(".meme-generator img");
   const memeTitle = document.querySelector(".meme-generator .meme-title");
   const memeAuthor = document.querySelector(".meme-generator .meme-author");
+  const loadingText = document.querySelector(".meme-generator .loading-text");
+
+
+  const showLoading = () => {
+    loadingText.style.display = "block";
+    memeImage.style.display = "none";
+    memeTitle.textContent = "";
+    memeAuthor.textContent = "";
+  };
+  
+  const hideLoading = () => {
+    loadingText.style.display = "none";
+    memeImage.style.display = "block";
+  };
   
   const updateDetails = (url, title, author) => {
     memeImage.setAttribute("src", url);
     memeTitle.innerHTML = title;
     memeAuthor.innerHTML = `Meme by: ${author}`;
+    hideLoading();
   };
   
   const generateMeme = () => {
+    showLoading();
     fetch("https://meme-api.com/gimme/wholesomememes")
       .then((response) => response.json())
       .then((data) => {
